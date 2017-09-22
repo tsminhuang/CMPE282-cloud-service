7ea03c5d24e3d94fbf044982b20424d41420ed10
Tsungmin Huang <tsungmin.huang@sjsu.edu>
2017-09-21 21:27:04 -0700

    Add restful nosql lab proejct skeleton

---
+++ restful-nosql/src/main/java/edu/sjsu/cmpe282/restfulnosql/RestfulNosqlApplication.java
@@ -0,0 +1,12 @@
+package edu.sjsu.cmpe282.restfulnosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulNosqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulNosqlApplication.class, args);
+	}
+}
